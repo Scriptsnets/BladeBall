df526d35f60d098623f8280b35c8745185a2884e
Scriptsnet <maramarajadematthew@gmail.com>
2026-02-03 03:24:55 +0800

    Vyzens.lua

---
+++ Vyzens.lua
@@ -0,0 +1,1 @@
+task.spawn(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Scriptsnets/BladeBall/refs/heads/main/Blade-Ball-Settings"))()end)loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaXOTTIC/BladeBall/refs/heads/main/VyzenBladeBallScript.lua"))()
