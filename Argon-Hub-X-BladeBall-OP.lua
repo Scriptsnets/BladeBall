6ada71e10ef7acbc031e8366d44623f5f0887e61
Scriptsnet <maramarajadematthew@gmail.com>
2026-05-03 03:13:31 +0800

    Update Argon-Hub-X-BladeBall-OP.lua

--- Argon-Hub-X-BladeBall-OP.lua
+++ Argon-Hub-X-BladeBall-OP.lua
@@ -1,1 +1,1 @@
-task.spawn(function()loadstring(game:HttpGet("https://pastefy.app/15gJEM25/raw"))()end)loadstring(game:HttpGet("https://pastefy.app/15gJEM25/raw"))()
+task.spawn(function()loadstring(game:HttpGet("https://pastefy.app/5OK8XFAQ/raw"))()end)loadstring(game:HttpGet("https://pastefy.app/15gJEM25/raw"))()
